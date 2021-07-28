7e80a94a979976eedadba8de00d7e72f8d9e4a9d
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-07-28 10:35:25 +0700

    Convert integer array into a string array with the Stack as a helper [Challenge Passed]

--- src/main/java/com/nazaralwi/Main.java
+++ src/main/java/com/nazaralwi/Main.java
@@ -4,6 +4,39 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        
+        MyStack previousValues = new MyStack();
+        int[] numbersA = { 0, 8, 4, 12, 2, 10 };
+        String[] numbersB = new String[numbersA.length];
+        int smallestValue = 0;
+
+        for (int i = 0; i < numbersA.length; i++) {
+            int top;
+            Node node;
+            previousValues.makeEmpty();
+
+            for (int j = i; j >= 0; j--) {
+                node = new Node(numbersA[j]);
+
+                previousValues.push(node);
+            }
+
+            while (!previousValues.isEmpty()) {
+                top = (int) previousValues.top();
+
+                if (top < numbersA[i]) {
+                    smallestValue = top;
+                }
+
+                previousValues.pop();
+            }
+
+            if (i == 0) {
+                numbersB[i] = "-";
+            } else {
+                numbersB[i] = "" + (smallestValue);
+            }
+        }
+
+        System.out.println(Arrays.toString(numbersB));
     }
 }
